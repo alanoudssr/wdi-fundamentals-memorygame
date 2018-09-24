c13621bdd717300974124ca4f58f6eeda54a7462
alanoud <alanoudssr@gmail.com>
2018-09-25 00:01:14 +0300

    created game logic

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -1,4 +1,6 @@
 
+//create cards array and store cards objects into it
+
 var cards = [
 {
 	rank: "queen",
@@ -21,26 +23,79 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 }
 ];
+
 var cardsInPlay = [];
 
+/*if uncommented: create sa shuffle function that will change 
+the cards placement every time the board is created 
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+*/
+
+
+//checks whether the cards match or not
+
 var checkForMatch = function(){
+	var scoreMessage = document.getElementById('resultMessage');
+	var scoreCount = document.getElementById('numberOfWins');
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+
+/* if a simple alert box is wanted to display the result 
+message this code could be uncommented:
+alert("You found a match!"); */
+
+		scoreMessage.innerHTML="You found a match!";
+	   document.getElementById('scoreBlock').appendChild(scoreMessage); 
 	}
 	else {
-		alert("Sorry, try again.");
+
+/* if a simple alert box is wanted to display the result 
+message this code could be uncommented:
+alert("Sorry, try again."); */
+
+		scoreMessage.innerHTML="Sorry, try again!";
+	   document.getElementById('scoreBlock').appendChild(scoreMessage); 
+/*I have chosen to display the score message differently 
+in order to allow the 2nd card to be flipped before the 
+message is displayed, which is something the alert box prevented*/
 	};
 };
 
+//flips card when clicked and calls the checkForMatch function
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped "+cards[cardId].rank)
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);	
-	checkForMatch();
+	console.log(cards[cardId].suit);
+	this.setAttribute('src',cards[cardId].cardImage);
+	if (cardsInPlay.length === 2){	
+	checkForMatch(); };
 	};
 
-flipCard(0);
-flipCard(2);
+//creates the board of cards everytime the page is loaded
 
+var createBoard = function(){
+	/*if uncommented calls the shuffle function before creating the board
+	shuffle(cards); */
+	for (var i = 0; i < cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src','images/back.png');
+cardElement.setAttribute('data-id',i);
+cardElement.addEventListener('click',flipCard);
+document.getElementById('game-board').appendChild(cardElement);
+};
+};
+
+//calls the createBoard function
+createBoard();
